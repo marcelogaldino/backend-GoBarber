d92b55f2638c836e4249aeb3120df6a80e6ce770
marcelogaldino <marcelinhomgo@gmail.com>
2020-12-01 16:53:40 -0300

    typeorm relationship between Users and Appointments

---
+++ src/routes/users.routes.ts
@@ -0,0 +1,38 @@
+import { Router } from 'express';
+import { hash } from 'bcryptjs';
+
+import CreateUserService from '../service/CreateUserService';
+
+interface UserInterface {
+    password?: string;
+}
+
+const usersRoutes = Router();
+
+usersRoutes.get('/', async (request, response) => {
+    return response.send('ok');
+});
+
+usersRoutes.post('/', async (request, response) => {
+    try {
+        const { name, email, password } = request.body;
+
+        const createUser = new CreateUserService();
+
+        const hashedPassword = await hash(password, 8);
+
+        const user = await createUser.execute({
+            name,
+            email,
+            password: hashedPassword,
+        });
+
+        // delete user.password;
+
+        return response.json(user);
+    } catch (error) {
+        return response.status(400).json({ error: error.message });
+    }
+});
+
+export default usersRoutes;
